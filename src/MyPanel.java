a3fb591bc2be3e026da1c2b831927360f479f8d9
Windows-7 <Windows-7@Windows-7-PC>
2017-03-21 10:02:49 -0500

    Changes in MyPanel: added mines to grid and changes to prevent compilation errors
    Changes in Adapter: made changes to prevent compilation errors

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,44 +1,72 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+
 import java.awt.Insets;
+
+
 import java.util.Random;
 
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
+	
 	private static final long serialVersionUID = 3426940946811133635L;
 	private static final int GRID_X = 25;
 	private static final int GRID_Y = 25;
 	private static final int INNER_CELL_SIZE = 29;
-	private static final int TOTAL_COLUMNS = 11;
-	private static final int TOTAL_ROWS = 11;   //Last row has only one cell
+	private static final int TOTAL_COLUMNS = 9;
+	private static final int TOTAL_ROWS = 9;   //Last row has only one cell
+
+	public int numberMinesEasy = 15;
+	public boolean minesOnField[][] = new boolean[TOTAL_COLUMNS][TOTAL_ROWS];
+	public String[][] numGrid = new String[TOTAL_COLUMNS][TOTAL_ROWS];
+
 	public int x = -1;
 	public int y = -1;
 	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
-	public Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
+	public Color[][] mineField = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
 	public MyPanel() {   //This is the constructor... this code runs first to initialize
-		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {	//Use of "random" to prevent unwanted Eclipse warning
+
+		
+		//Generates the mines for the Field
+		//If a tile is already a mine, it generates a new mine in a different tile
+		for (int i = 0; i < numberMinesEasy; i++){
+			for (int j = 0; j < 1; j++){
+				Random generator = new Random();
+				int x = generator.nextInt(TOTAL_COLUMNS);
+				int y = generator.nextInt(TOTAL_ROWS);
+
+				if(minesOnField[x][y] == false){
+					minesOnField[x][y] = true;
+				}
+				else {
+					i--;
+				}
+			}
+		}
+		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {    //Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("INNER_CELL_SIZE must be positive!");
 		}
-		if (TOTAL_COLUMNS + (new Random()).nextInt(1) < 2) {	//Use of "random" to prevent unwanted Eclipse warning
+		if (TOTAL_COLUMNS + (new Random()).nextInt(1) < 2) {    //Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("TOTAL_COLUMNS must be at least 2!");
 		}
-		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {	//Use of "random" to prevent unwanted Eclipse warning
+		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {    //Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("TOTAL_ROWS must be at least 3!");
 		}
-		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Top row
-			colorArray[x][0] = Color.LIGHT_GRAY;
+		for (int x = 0; x < TOTAL_COLUMNS; x++) { 
+			mineField[x][0] = Color.WHITE;
+			numGrid[x][0] =  ""; 
 		}
-		for (int x = 0; x < TOTAL_COLUMNS; x++){
-			colorArray[x][10] = Color.LIGHT_GRAY;
+		for (int y = 0; y < TOTAL_ROWS; y++) { 
+			mineField[0][y] = Color.WHITE;
+			numGrid[0][y] =  ""; 
 		}
-		for (int y = 0; y < TOTAL_ROWS; y++) {   //Left column
-			colorArray[0][y] = Color.LIGHT_GRAY;
-		}
-		for (int x = 1; x < TOTAL_COLUMNS - 1; x++) {   //The rest of the grid
-			for (int y = 1; y < TOTAL_ROWS - 1; y++) {
-				colorArray[x][y] = Color.GRAY;
+		for (int x = 1; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
+			for (int y = 1; y < TOTAL_ROWS; y++) {
+				mineField[x][y] = Color.WHITE;
+				numGrid[x][y] =  ""; 
 			}
 		}
 	}
@@ -49,35 +77,34 @@ public class MyPanel extends JPanel {
 		Insets myInsets = getInsets();
 		int x1 = myInsets.left;
 		int y1 = myInsets.top;
-		int x2 = getWidth() - myInsets.right - 1;
-		int y2 = getHeight() - myInsets.bottom - 1;
-		int width = x2 - x1;
-		int height = y2 - y1;
+		//int x2 = getWidth() - myInsets.right - 1;
+		//int y2 = getHeight() - myInsets.bottom - 1;
+		//int width = x2 - x1;
+		//int height = y2 - y1;
 
-		//Paint the background
-		g.setColor(Color.LIGHT_GRAY);
-		g.fillRect(x1, y1, width + 1, height + 1);
+		
 
+		
 		//Draw the grid minus the bottom row (which has only one cell)
-		//By default, the grid will be 10x10 (see above: TOTAL_COLUMNS and TOTAL_ROWS) 
+		//By default, the grid will be 9x9 (see above: TOTAL_COLUMNS and TOTAL_ROWS)
 		g.setColor(Color.BLACK);
-		for (int y = 1; y <= TOTAL_ROWS - 1; y++) {
-			g.drawLine(x1 + GRID_X + INNER_CELL_SIZE + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X - 9 + ((INNER_CELL_SIZE - 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
+		for (int y = 0; y <= TOTAL_ROWS; y++) {
+			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
 		}
-		for (int x = 1; x <= TOTAL_COLUMNS - 1; x++) {
-			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + INNER_CELL_SIZE + 1, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y - 8 + ((INNER_CELL_SIZE - 1) * (TOTAL_ROWS)));
+		for (int x = 0; x < TOTAL_COLUMNS + 1; x++) {
+			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS)));
 		}
-
-		//Draw an additional cell at the bottom left
-		//g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
-
-		//Paint cell colors
+		//Paint cell colors and writes the number of mines near the tiles
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
 			for (int y = 0; y < TOTAL_ROWS; y++) {
-				if ((x == 0) || (y != TOTAL_ROWS - 1)) {
-					Color c = colorArray[x][y];
+				if ((x == 0) || (y != TOTAL_ROWS)) {
+					Color c = mineField[x][y];
 					g.setColor(c);
 					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
+					Color purple =  new Color(153, 0, 153);
+					g.setColor(purple);
+					g.setFont(new Font("Goudy Stout", Font.PLAIN, 14));
+					g.drawString(numGrid[x][y], x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 10, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 20);
 				}
 			}
 		}
@@ -99,10 +126,10 @@ public class MyPanel extends JPanel {
 		}
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
-		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
+		if (x == 0 && y == TOTAL_ROWS ) {    //The lower left extra cell
 			return x;
 		}
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS  || y < 0 || y > TOTAL_ROWS) {   //Outside the rest of the grid
 			return -1;
 		}
 		return x;
@@ -124,12 +151,20 @@ public class MyPanel extends JPanel {
 		}
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
-		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
+		if (x == 0 && y == TOTAL_ROWS ) {    //The lower left extra cell
 			return y;
 		}
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS || y < 0 || y > TOTAL_ROWS) {   //Outside the rest of the grid
 			return -1;
 		}
 		return y;
+	}
+	
+	//Determines if a tile is a mine or not
+	public boolean isMine(int x, int y){
+		if(minesOnField[x][y] == true){
+			return true;
+		}
+		return false;
 	}
 }
